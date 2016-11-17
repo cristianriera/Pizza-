724716bbefde496b0202b969bc60640fa26a9d58
cristianriera <7criera@stpatrick.org>
2016-11-17 10:50:38 -0600

    Add files via upload
    Started Constructor

---
+++ pizza.js
@@ -0,0 +1,17 @@
+class pizza{
+  //type constructor below
+  constructor(s,mt,vt){
+    this.size = s;
+    this.meatToppings = mt;
+    this.veggieToppings = vt;
+  }
+
+
+  //type instance functions below
+
+
+
+  //type class functions below
+
+
+}
