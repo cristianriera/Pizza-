0cab304bfae069cff8858a5a8b56f84ff9c33b3b
cristianriera <7criera@stpatrick.org>
2016-11-18 10:47:27 -0600

    Add files via upload
    Added size cost

--- pizza.js
+++ pizza.js
@@ -5,7 +5,26 @@ class pizza{
     this.meatToppings = mt;
     this.veggieToppings = vt;
   }
-
+  sizeCost(){
+    if(this.size == "Small"){
+      this.sizeCost = 7.99;
+      return sizeCost;
+    }
+    else if(this.size == "Medium"){
+      this.sizeCost = 9.99;
+      return sizeCost;
+    }
+    else if(this.size == "Large"){
+      this.sizeCost = 12.99;
+      return sizeCost;
+    }
+    else if(this.size == "Extra-Large"){
+      this.sizeCost = 15.99;
+      return sizeCost;
+    }
+  }
+  toppingCost(){
+  }
 
   //type instance functions below
 
